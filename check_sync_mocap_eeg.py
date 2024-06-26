7196151990949f7af257f3b82129575bbd764c01
Julius Welzel <julius.welzel@gmail.com>
2024-06-26 17:10:23 +0200

    fix file names

--- check_sync_mocap_eeg.py
+++ check_sync_mocap_eeg.py
@@ -6,14 +6,14 @@ from scipy.signal import find_peaks
 
 
 # Load in the data
-data, header = pyxdf.load_xdf('data/test.xdf')
+data, header = pyxdf.load_xdf('data/sync_mocap_eeg_motion.xdf')
 
 nms_streams = [stream["info"]["name"][0] for stream in data]
 
 mocap_times = data[0]["time_stamps"] -  data[0]["time_stamps"][0]
 mocap_raw = data[0]["time_series"]
 
-eeg_times = data[1]["time_stamps"] - data[1]["time_stamps"][0]
+eeg_times = data[1]["time_stamps"] - data[0]["time_stamps"][1]
 eeg_raw = data[1]["time_series"]
 
 peak_eeg = find_peaks(zscore(eeg_raw[:,1]), distance=2)
