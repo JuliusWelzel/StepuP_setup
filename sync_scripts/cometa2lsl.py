af9801d1e89f4a18069d43bc43f62156223089d3
jwelzel <52565341+JuliusWelzel@users.noreply.github.com>
2024-10-28 10:09:41 +0100

    fix smaller issues for debugging

--- sync_scripts/cometa2lsl.py
+++ sync_scripts/cometa2lsl.py
@@ -2,51 +2,39 @@ import clr
 import os
 import sys
 
+#####
+# Get system setup
+#####
+
 python_dll_path = r"C:\franc\anaconda3\python39.dll"  # Modify this path according to your Python installation
 os.environ["PYTHONNET_PYDLL"] = python_dll_path
 
 sys.path.append(r"C:\Users\franc\Documents\IRCCS-ISNB\StepuP\Instrumentation\SDK\WaveX SDK")
 a = clr.AddReference("WaveX")
 
-import os
 path = r"C:\Users\franc\Documents\IRCCS-ISNB\StepuP\Instrumentation\SDK\WaveX SDK\WaveX.dll"
-print(os.path.exists(path))  # expected outpuut: True
+if os.path.exists(path):
+    print(f"The DLL file was found at: {path}")
+else:
+    print(f"Error: The DLL file was not found at: {path}")
+
+
+#####
+# load the DLL
+#####
 
 from System.Reflection import Assembly
-# load the DLL
 assembly = Assembly.LoadFile(path)
 
-# Stampa i nomi dei tipi disponibili
+# Print available type names
 for type in assembly.GetTypes():
     print(type.FullName)
     
-
 from System import AppDomain
-# Mostra tutte le classi nel modulo WaveX
+# Show all classes in the WaveX module
 waveXAssembly = AppDomain.CurrentDomain.Load("WaveX")
 for type in waveXAssembly.GetTypes():
     print(type.FullName)
-
-# from WaveX import DaqSystem
-#import WaveX
-from WaveX import *
-import matplotlib.pyplot as plt
-import pylsl as lsl
-import numpy as np
-import pyxdf
-import socket
-
-srate_emg = 2000# sampling rate of the EMG sensors, can be edited at the base station
-s_buffer = 1 # seconds to buffer and send to LSL
-N_chns_emg = 8 # number of channels, 6 is minimum for Stepup
-samples_per_read = int(srate_emg * s_buffer)
-
-# create lsl outlet
-info = lsl.StreamInfo('Cometa', 'EMG', N_chns_emg,  srate_emg, 'float32', 'myuid34234')
-outlet = lsl.StreamOutlet(info, chunk_size=samples_per_read, max_buffered=120) 
-
-from System.Reflection import Assembly
-from System import Activator, EventHandler
 
 daq_system_type = assembly.GetType('WaveX.DaqSystem')
 
@@ -57,19 +45,34 @@ if daq_system_type and daq_system_type.IsClass:
 else:
     print("DaqSystem not found or is not a class in the loaded assembly.")
 
+# from load LSL relevant packages
+import matplotlib.pyplot as plt
+import pylsl as lsl
+import numpy as np
 
-sensors = daq_system.InstalledSensors
-print(f"Numero di sensori connessi: {sensors}")
+# setup CONSTNATS for LSL
+SRATE_EMG = 2000 # sampling rate of the EMG sensors, can be edited at the base station
+BUFFER_LENGTH_SEC = 1 # seconds to buffer and send to LSL
+N_CHANS_EMG = 8 # number of channels, 6 is minimum for Stepup
+SAMPLES_PER_READ = int(SRATE_EMG * BUFFER_LENGTH_SEC)
 
-#Configure the sensors
-capture_config = daq_system.CaptureConfiguration()
+# create lsl outlet
+info = lsl.StreamInfo('Cometa', 'EMG', N_CHANS_EMG,  SRATE_EMG, 'float32', 'myuid34234')
+outlet = lsl.StreamOutlet(info, chunk_size=SAMPLES_PER_READ, max_buffered=120) 
 
-    
+
+# Connect and configure the sensors    
 from WaveX.Common.Definitions import EmgAcqXType
 from WaveX.Common.Definitions import DataAvailableEventPeriod
 from WaveX.Common.Definitions import DataAvailableEventArgs
 
-capture_config.EMG_AcqXType = EmgAcqXType.Emg_2kHz
+sensors = daq_system.InstalledSensors
+print(f"Number of connected sensors: {sensors}")
+
+#Configure the sensors
+capture_config = daq_system.CaptureConfiguration()
+
+capture_config.EMG_AcqXType = EmgAcqXType.Emg_2kHz # set sampling rate to 2 kHz
 
 daq_system.ConfigureCapture(capture_config)
 daq_system.EnableSensor(0) # Enable all sensors 
@@ -89,12 +92,12 @@ def dataAvailableHandler(source, args: DataAvailableEventArgs):
 
     # Convert the samples to a NumPy array for all channels
     emg_data_np = np.array(args.EmgSamples)  # Get all samples for streaming
-    
+
+    # print type of args.EmgSamples
+    # Print the type and shape of the EMG samples for debugging
+    print(f"Type of EMG samples: {type(args.EmgSamples)}")
+    print(f"Shape of EMG samples: {np.shape(args.EmgSamples)}")
     data_counter += 1
-    
-    # Print the data shape for debugging
-    print(f"Data shape before pushing to LSL: {np.shape(emg_data_np.T)}")
-    print(f"Data before pushing to LSL: {emg_data_np.T}")
 
     # Push the entire dataset to the LSL outlet
     outlet.push_chunk(emg_data_np.T)  # Transpose if needed for LSL format
