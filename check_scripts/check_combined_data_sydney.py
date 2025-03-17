01cc186ab295e42589b535d03697c89ef15fe84d
jwelzel <52565341+JuliusWelzel@users.noreply.github.com>
2025-03-17 13:37:33 +0100

    [UPDATE] modify file path in check_combined_data_sydney.py; enhance mocap and EMG data extraction and visualization

--- check_scripts/check_combined_data_sydney.py
+++ check_scripts/check_combined_data_sydney.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pyxdf
 
-file_path = r"C:\Users\User\Desktop\kiel\stepup\stepup_setup_jw\data\test_sydney_110325\MaynaTestWalk01.xdf"  # Replace with your XDF file path
+file_path = r"C:\Users\juliu\Desktop\kiel\stepup_setup_jw\data\test_sydney_110325\MaynaTestWalk01.xdf"  # Replace with your XDF file path
 
 streams, fileheader = pyxdf.load_xdf(file_path)
 print("File loaded successfully.")
@@ -31,42 +31,34 @@ mocap_raw = mocap_stream['time_series']
 eeg_times = eeg_stream['time_stamps'] - eeg_stream['time_stamps'][0]
 eeg_raw = eeg_stream['time_series']
 
+# information about the columns
+# <Column A>Timestamps
+# <Colum B(0)>FrameNumbers
+# <Colum (1)C>Frame rate
+# <Colum D(2)>Total Time that the loop has taken for capturing the data from Vicon(It's just for our understanding, don't worry about it)
+# <Colum E(3)-CF(82)>Vicon Data<Colum BM(63)-BT(70)>Left Foot and Right Foot(Extra Markers)<Colum BU(71)-CF(82)>Unlabeled Markers>
+# <Colum CG(83)-AAV(722)EMG Data>We used only two sensors, that 'why all other columns are zero
+# <Colum AAW(723)-AHV(904)>Force Plates Data
+
 # extract mocap marker data
-
-
-# print unique marker ids, and number of occurences per makrer id
-marker_ids = mocap_raw[0,:]
-unique_marker_ids = set(marker_ids)
-for marker_id in unique_marker_ids:
-    print(f"Marker {int(marker_id)}: {np.sum(marker_ids == marker_id)}")
-    
-# remove all markers which have less than 100 frames
-for marker_id in unique_marker_ids:
-    if np.sum(marker_ids == marker_id) < 150:
-        idx = np.where(marker_ids == marker_id)
-        mocap_raw = np.delete(mocap_raw, idx, axis=0)
-        marker_ids = np.delete(marker_ids, idx)
-        
-# extract only marker 503
-idx_34 = np.where(mocap_raw[:,3] == 2)
-idx_294 = np.where(mocap_raw[:,3] == 3)
-idx_296 = np.where(mocap_raw[:,3] == 4)
-marker_34 = mocap_raw[idx_34][:,0:3]
-marker_294 = mocap_raw[idx_294][:,0:3]
-marker_296 = mocap_raw[idx_296][:,0:3]
+mocap_times = mocap_raw[:,0]
+frames_mocap = mocap_raw[:,1]
+vicon_data = mocap_raw[:,3:83]
+lf_z_vicon = mocap_raw[:,65]
+emg_data = mocap_raw[:,83:723]
+some_emg = emg_data[:,0]
 
 # make 3 subplots
 fig, axs = plt.subplots(3, 1, sharex=True)
 # plot 1 emg
-axs[0].plot(emg_times, emg_raw) 
+axs[0].plot(mocap_times, some_emg) 
 axs[0].set_ylabel('EMG')
-axs[0].set_ylim([-.7,.5])
+#axs[0].set_ylim([-.7,.5])
 #axs[0].legend(['RfEmgR', 'BfEmgR', 'RfEmgL', 'BfEmgL'])
 
 # plot 2 mocap
 #axs[1].plot(mocap_times[idx_34], marker_34[:,2])
-axs[1].plot(mocap_times[idx_294], marker_294[:,2])
-axs[1].plot(mocap_times[idx_296], marker_296[:,2])
+axs[1].plot(mocap_times, lf_z_vicon)
 axs[1].set_xlabel('Time (s)')
 axs[1].set_ylabel('Z position (?)')
 #axs[1].legend(['Marker 34', 'Marker 294', 'Marker 296'])
@@ -76,7 +68,8 @@ axs[1].set_ylabel('Z position (?)')
 axs[2].plot(eeg_times, eeg_stream['time_series'][:,0])
 axs[2].set_ylabel('EEG')
 axs[2].set_xlabel('Time (s)')
+axs[2].set_ylim([-100, 100])
 
 # set xlim 20-30s for all subplots
 for ax in axs:
-    ax.set_xlim([250, 255])
+    ax.set_xlim([10, 25])
